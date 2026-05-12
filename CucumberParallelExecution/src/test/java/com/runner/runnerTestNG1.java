dc5bb03d6384fd2f0af85a2ea95f19e7b3619e38
aarun83 <109081355+aarun83@users.noreply.github.com>
2026-05-12 15:38:02 +0530

    Update runnerTestNG1.java

--- CucumberParallelExecution/src/test/java/com/runner/runnerTestNG1.java
+++ CucumberParallelExecution/src/test/java/com/runner/runnerTestNG1.java
@@ -6,12 +6,12 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-         features = "src/test/resources/features/LoginPage1.feature",
+         features = "src/test/resources/features",
          glue = "com.definitions1",
          plugin = {
         		 "pretty", 
-        		 "html:target/cucumber-reports.html", 
-        		 "json:target/cucumber.json", 
+        		 "html:target/cucumber-reports/cucumber.html", 
+        		 "json:target/cucumber-reports/cucumber.json", 
         		 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
          },
          monochrome = true)
